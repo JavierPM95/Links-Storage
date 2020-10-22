df735f9b7d6f77f85d1ab66618d9182405b4fce0
JavierPM1995 <jaavierpm1995@gmail.com>
2020-10-22 17:30:48 -0300

    Updates

---
+++ precache-manifest.39f03539ccaffcd60b883e49c22c8836.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f114648f49182b5eae9bff86a147402",
+    "url": "/Links-Storage/index.html"
+  },
+  {
+    "revision": "6e7be4e11f5e7660824d",
+    "url": "/Links-Storage/static/css/2.6d3f2af7.chunk.css"
+  },
+  {
+    "revision": "3a564c7a1deb6db74135",
+    "url": "/Links-Storage/static/css/main.b53f1ed6.chunk.css"
+  },
+  {
+    "revision": "6e7be4e11f5e7660824d",
+    "url": "/Links-Storage/static/js/2.74b247f8.chunk.js"
+  },
+  {
+    "revision": "659df5b420806b55437f4f9e57f60411",
+    "url": "/Links-Storage/static/js/2.74b247f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a564c7a1deb6db74135",
+    "url": "/Links-Storage/static/js/main.e2e3815c.chunk.js"
+  },
+  {
+    "revision": "fbc809c78ad5e075229c",
+    "url": "/Links-Storage/static/js/runtime-main.30783679.js"
+  }
+]);
